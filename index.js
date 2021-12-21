687d4587fbe6dc52f72450632c1c8842e31d7f51
Andrew Yu <andrewyu209@gmail.com>
2021-12-20 20:00:19 -0500

    Added Role Function

--- index.js
+++ index.js
@@ -2,7 +2,52 @@ const inquirer = require('inquirer');
 const scripts = require('./lib/script');
 require('console.table');
 
+const addDept = () => {
+    return inquirer.prompt({
+            type: 'input',
+            name: 'department',
+            message: 'Please enter a new Department name: '
+        })
+        .then(newDept => {
+            scripts.addDepartment(newDept.department);
+            return options();
+        })
+}
 
+const addRole = () => {
+    let dataSet;
+    scripts.viewAllDepartments().then(data => {
+            dataSet = data;
+            return data.map(x => { return x.name });
+        })
+        .then(dept => {
+            return inquirer.prompt([{
+                    type: 'input',
+                    name: 'role',
+                    message: 'Please enter a new Role: '
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'Please enter the salary of the role: '
+                },
+                {
+                    type: 'list',
+                    name: 'dept',
+                    message: 'Please select a Department',
+                    choices: dept
+                }
+            ]).then(answers => {
+                let deptID = dataSet.filter(x => {
+                    if (x.name === answers.dept) {
+                        return x.id;
+                    }
+                })[0].id;
+                scripts.addRole(answers.role, answers.salary, deptID);
+                return options();
+            });
+        })
+}
 
 const options = () => {
     return inquirer.prompt([
@@ -17,13 +62,32 @@ const options = () => {
         .then(answer => {
             switch (answer.option) {
                 case 'View all Departments':
-                    scripts.viewAllDepartments().then(data => console.table(data)).then(options);
+                    scripts.viewAllDepartments().then(data => {
+                        console.table(data)
+                        options();
+                    });
                     break;
                 case 'View all Roles':
-                    scripts.viewAllRoles().then(data => console.table(data)).then(options);
+                    scripts.viewAllRoles().then(data => {
+                        console.table(data)
+                        options();
+                    });
                     break;
                 case 'View all Employees':
-                    scripts.viewAllEmployees().then(data => console.table(data)).then(options);
+                    scripts.viewAllEmployees().then(data => {
+                        console.table(data)
+                        options();
+                    });
+                    break;
+                case 'Add Department':
+                    addDept();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'Add Employee':
+                    break;
+                case 'Update Employee Role':
                     break;
             }
         })
