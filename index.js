02c8d53539bd690f85cfd46d77050ac0598d5c97
Andrew Yu <andrewyu209@gmail.com>
2021-12-22 18:39:30 -0500

    Add Employee Function

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const inquirer = require('inquirer');
 const scripts = require('./lib/script');
 require('console.table');
 
+async function getDepartment() {
+    let deptName = await scripts.viewAllDepartments();
+    return deptName;
+}
+
+async function getRole() {
+    let roleName = await scripts.viewAllRoles();
+    return roleName
+}
+
+async function getEmployee() {
+    let allEmployee = await scripts.viewAllEmployees();
+    return allEmployee;
+
+}
+
 const addDept = () => {
     return inquirer.prompt({
             type: 'input',
@@ -15,66 +31,137 @@ const addDept = () => {
 }
 
 const addRole = () => {
-    let dataSet;
-    scripts.viewAllDepartments().then(data => {
-            dataSet = data;
-            return data.map(x => { return x.name });
+    let department = [];
+    getDepartment().then(dept => {
+        dept.forEach(x => {
+            department.push(x)
         })
-        .then(dept => {
-            return inquirer.prompt([{
-                    type: 'input',
-                    name: 'role',
-                    message: 'Please enter a new Role: '
-                },
-                {
-                    type: 'input',
-                    name: 'salary',
-                    message: 'Please enter the salary of the role: '
-                },
-                {
-                    type: 'list',
-                    name: 'dept',
-                    message: 'Please select a Department',
-                    choices: dept
+    });
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'role',
+            message: 'Please enter a new Role: '
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Please enter the salary of the role: '
+        },
+        {
+            type: 'rawlist',
+            name: 'dept',
+            message: 'Please select a Department',
+            choices: department
+        }
+    ]).then(answers => {
+        let deptID = department.find(x => {
+            if (x.name === answers.dept) {
+                return x.id
+            }
+            return;
+        }).id
+        scripts.addRole(answers.role, answers.salary, deptID);
+        return options();
+    });
+}
+
+const addEmployee = () => {
+    // let allRole;
+    // let allEmployee;
+    // scripts.viewAllRoles().then(data => {
+    //     allRole = data;
+    //     scripts.viewAllEmployees().then(employee => {
+    //         allEmployee = employee;
+    //         console.log(allEmployee)
+    //     });
+    //     return data.map(x => { return x.Title });
+    // }).then(roles => {
+    let role = [];
+    let employee = [];
+    getRole().then(roles => {
+        roles.forEach(x => {
+            let newObj = {
+                id: x.id,
+                name: x.Title
+            }
+            role.push(newObj);
+        })
+    })
+    getEmployee().then(employees => {
+        employees.forEach(x => {
+            let newObj = {
+                id: x.id,
+                name: x.first_name + " " + x.last_name
+            }
+            employee.push(newObj);
+        })
+        employee.push({ id: null, name: "No Manager" });
+    })
+    return inquirer.prompt([{
+                type: 'input',
+                name: 'first',
+                message: "Please enter employee's first name: "
+            },
+            {
+                type: 'input',
+                name: 'last',
+                message: "Please enter employee's last name: "
+            },
+            {
+                type: 'rawlist',
+                name: 'role',
+                message: "Please select employee's role: ",
+                choices: role
+            },
+            {
+                type: 'rawlist',
+                name: 'manager',
+                message: "Please select employee's manager: ",
+                choices: employee
+            }
+        ]).then(answer => {
+            let roleID = role.find(x => {
+                if (x.name === answer.role) {
+                    return x;
                 }
-            ]).then(answers => {
-                let deptID = dataSet.filter(x => {
-                    if (x.name === answers.dept) {
-                        return x.id;
-                    }
-                })[0].id;
-                scripts.addRole(answers.role, answers.salary, deptID);
-                return options();
-            });
+            }).id
+            let managerID = employee.find(x => {
+                if (x.name === answer.manager) {
+                    return x;
+                }
+            }).id;
+            scripts.addEmployee(answer.first, answer.last, roleID, managerID);
+            return options();
         })
+        // })
 }
 
 const options = () => {
     return inquirer.prompt([
             // Select Options
             {
-                type: 'list',
+                type: 'rawlist',
                 name: 'option',
                 message: 'Select an option!',
-                choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role']
+                choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'End']
             }
         ])
         .then(answer => {
             switch (answer.option) {
                 case 'View all Departments':
-                    scripts.viewAllDepartments().then(data => {
+                    getDepartment().then(data => {
                         console.table(data)
                         options();
                     });
                     break;
                 case 'View all Roles':
-                    scripts.viewAllRoles().then(data => {
+                    getRole().then(data => {
                         console.table(data)
                         options();
                     });
                     break;
                 case 'View all Employees':
-                    scripts.viewAllEmployees().then(data => {
+                    getEmployee().then(data => {
                         console.table(data)
                         options();
                     });
@@ -86,9 +173,12 @@ const options = () => {
                     addRole();
                     break;
                 case 'Add Employee':
+                    addEmployee();
                     break;
                 case 'Update Employee Role':
                     break;
+                case 'End':
+                    process.exit();
             }
         })
 }
