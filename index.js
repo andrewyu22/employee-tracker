7efeb391de26a4bb9503c4bdc5228d38c5783a02
Andrew Yu <andrewyu209@gmail.com>
2021-12-22 19:30:50 -0500

    Added comments to index.js

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ const inquirer = require('inquirer');
 const scripts = require('./lib/script');
 require('console.table');
 
+// Get All Department Data from DB
 async function getDepartment() {
+    // Calls the Database Class Functions & return an array of object
     let deptName = await scripts.viewAllDepartments();
     return deptName;
 }
 
+// Get All Role Data from DB
 async function getRole() {
+    // Calls the Database Class Functions & return an array of object
     let roleName = await scripts.viewAllRoles();
     return roleName
 }
 
+// Get All Employee from DB
 async function getEmployee() {
+    // Calls the Database Class Functions & return an array of object
     let allEmployee = await scripts.viewAllEmployees();
     return allEmployee;
 
 }
 
+// Add new Department to the Database
 const addDept = () => {
-    return inquirer.prompt({
-            type: 'input',
-            name: 'department',
-            message: 'Please enter a new Department name: '
-        })
+    return inquirer.prompt(
+            // Prompt users to enter a new department
+            {
+                type: 'input',
+                name: 'department',
+                message: 'Please enter a new Department name: '
+            })
         .then(newDept => {
+            // Calls the Database Class Function and pass the newDept Paramter to add to the DB
             scripts.addDepartment(newDept.department);
             return options();
         })
@@ -32,21 +42,26 @@ const addDept = () => {
 
 const addRole = () => {
     let department = [];
+    // Get All Department and push the object into array called department
     getDepartment().then(dept => {
-        dept.forEach(x => {
-            department.push(x)
+        dept.forEach(deptName => {
+            department.push(deptName)
         })
     });
-    return inquirer.prompt([{
+    return inquirer.prompt([
+        // Prompt users for Role Name
+        {
             type: 'input',
             name: 'role',
             message: 'Please enter a new Role: '
         },
+        // Prompt users for the Role's Salary
         {
             type: 'input',
             name: 'salary',
             message: 'Please enter the salary of the role: '
         },
+        // Prompt users for Department
         {
             type: 'rawlist',
             name: 'dept',
@@ -54,86 +69,89 @@ const addRole = () => {
             choices: department
         }
     ]).then(answers => {
+        // Find the Department ID
         let deptID = department.find(x => {
-            if (x.name === answers.dept) {
-                return x.id
-            }
-            return;
-        }).id
+                if (x.name === answers.dept) {
+                    return x.id
+                }
+            }).id
+            // Calls the Database Class Function and pass Paramter to add the new role to DB
         scripts.addRole(answers.role, answers.salary, deptID);
         return options();
     });
 }
 
 const addEmployee = () => {
-    // let allRole;
-    // let allEmployee;
-    // scripts.viewAllRoles().then(data => {
-    //     allRole = data;
-    //     scripts.viewAllEmployees().then(employee => {
-    //         allEmployee = employee;
-    //         console.log(allEmployee)
-    //     });
-    //     return data.map(x => { return x.Title });
-    // }).then(roles => {
-    let role = [];
-    let employee = [];
+    let allRole = [];
+    let allEmployee = [];
+    // Get All Role and create a new Object with ID/Title and push into array called allRole
     getRole().then(roles => {
-        roles.forEach(x => {
+        roles.forEach(role => {
             let newObj = {
-                id: x.id,
-                name: x.Title
+                id: role.id,
+                name: role.Title
             }
-            role.push(newObj);
+            allRole.push(newObj);
         })
-    })
+    });
+    // Get All Employee and create a new Object with ID/Name and push into array called allEmployee
     getEmployee().then(employees => {
-        employees.forEach(x => {
-            let newObj = {
-                id: x.id,
-                name: x.first_name + " " + x.last_name
-            }
-            employee.push(newObj);
-        })
-        employee.push({ id: null, name: "No Manager" });
-    })
-    return inquirer.prompt([{
-                type: 'input',
-                name: 'first',
-                message: "Please enter employee's first name: "
-            },
-            {
-                type: 'input',
-                name: 'last',
-                message: "Please enter employee's last name: "
-            },
-            {
-                type: 'rawlist',
-                name: 'role',
-                message: "Please select employee's role: ",
-                choices: role
-            },
-            {
-                type: 'rawlist',
-                name: 'manager',
-                message: "Please select employee's manager: ",
-                choices: employee
-            }
-        ]).then(answer => {
-            let roleID = role.find(x => {
-                if (x.name === answer.role) {
-                    return x;
+        employees.forEach(employee => {
+                // Create a new object with only ID & Name
+                let newObj = {
+                        id: employee.id,
+                        name: employee.first_name + " " + employee.last_name
+                    }
+                    // Push object into allEmployee
+                allEmployee.push(newObj);
+            })
+            // Push 1 more option which is No Manager into allEmployee array
+        allEmployee.push({ id: null, name: "No Manager" });
+    });
+    return inquirer.prompt([
+        // Prompt users for First Name
+        {
+            type: 'input',
+            name: 'first',
+            message: "Please enter employee's first name: "
+        },
+        // Prompt user for Last Name
+        {
+            type: 'input',
+            name: 'last',
+            message: "Please enter employee's last name: "
+        },
+        // Select from List of Existing Roles
+        {
+            type: 'rawlist',
+            name: 'role',
+            message: "Please select employee's role: ",
+            choices: allRole
+        },
+        // Select from List of Existing Employee
+        {
+            type: 'rawlist',
+            name: 'manager',
+            message: "Please select employee's manager: ",
+            choices: allEmployee
+        }
+    ]).then(answer => {
+        // Find Role ID
+        let roleID = allRole.find(id => {
+                if (id.name === answer.role) {
+                    return id;
                 }
             }).id
-            let managerID = employee.find(x => {
-                if (x.name === answer.manager) {
-                    return x;
-                }
-            }).id;
-            scripts.addEmployee(answer.first, answer.last, roleID, managerID);
-            return options();
-        })
-        // })
+            // Find Manager ID
+        let managerID = allEmployee.find(id => {
+            if (id.name === answer.manager) {
+                return id;
+            }
+        }).id;
+        // Calls the Database Class Function and pass Paramter to add the new employee to DB
+        scripts.addEmployee(answer.first, answer.last, roleID, managerID);
+        return options();
+    })
 }
 
 const options = () => {
